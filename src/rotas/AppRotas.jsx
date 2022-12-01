6d792a91de25f42945b98a909383539d53488ebb
Luiz Carvalho <maximusmano@gmail.com>
2022-12-01 16:43:36 -0300

    finishing module 4

--- src/rotas/AppRotas.jsx
+++ src/rotas/AppRotas.jsx
@@ -5,13 +5,31 @@ import ProdutorRotas from './ProdutorRotas';
 import MelhoresProdutoresRotas from './MelhoresProdutoresRotas';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+import Coracao from '../assets/coracao.svg';
+import Home from '../assets/home.svg';
+
 
 const Tab = createBottomTabNavigator();
 
 function AppRotas() {
   return (
     <NavigationContainer>
-    <Tab.Navigator>
+    <Tab.Navigator
+    screenOptions={({route}) => ({
+      headerShown: false,
+      tabBarActiveTintColor: '#2A9F85',
+      tabBarInactiveTintColor: '#C7C7C7',
+      tabBarIcon: ({color}) => {
+        let Icon = Home;
+
+        if(route.name === 'MelhoresProdutores'){
+          Icon = Coracao;
+        }
+
+        return <Icon color={color} />;
+      }
+    })}
+    >
       <Tab.Screen name="ProdutorRotas" component={ProdutorRotas} />
       <Tab.Screen name="MelhoresProdutores" component={MelhoresProdutoresRotas} />
     </Tab.Navigator>
